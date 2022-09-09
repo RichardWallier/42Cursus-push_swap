ba7390831912a55ec33f03ae5ff1a578b372d4c5
Richard Wallier <rwallier@student.42.fr>
2022-09-09 17:00:37 -0300

    pa pb ra

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rwallier <rwallier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 18:36:46 by rwallier          #+#    #+#             */
-/*   Updated: 2022/09/05 19:12:05 by rwallier         ###   ########.fr       */
+/*   Updated: 2022/09/09 16:48:33 by rwallier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ int	choose_operation(char *operation, t_data *data)
 		swap_b(data);
 	else if (ft_strnstr(operation, "ss", ft_strlen(operation)))
 		swap_a_b(data);
+	else if (ft_strnstr(operation, "pa", ft_strlen(operation)))
+		push_a(data);
+	else if (ft_strnstr(operation, "pb", ft_strlen(operation)))
+		push_b(data);
+	else if (ft_strnstr(operation, "ra", ft_strlen(operation)))
+		rotate_a(data);
 	return (1);
 }
 
@@ -36,7 +42,7 @@ int	swap_a(t_data *data)
 	int	temp;
 
 	ft_printf("\n-----------operation: sa-------------");
-	if (data->stack_size < 2)
+	if (data->stack_size_a < 2)
 	{
 		ft_printf("Do nothing if there is only one or no elements.\n");
 		return (0);
@@ -52,7 +58,7 @@ int	swap_b(t_data *data)
 	int	temp;
 
 	ft_printf("\n-----------operation: sb-------------");
-	if (data->stack_size < 2)
+	if (data->stack_size_b < 2)
 	{
 		ft_printf("Do nothing if there is only one or no elements.\n");
 		return (0);
@@ -63,9 +69,83 @@ int	swap_b(t_data *data)
 	return (1);
 }
 
+int	push_b(t_data *data)
+{
+	int	index;
+
+	if (data->stack_size_a == 0)
+		return (0);
+	index = data->stack_size_b;
+	while (index > 0)
+	{
+		data->stack_b[index] = data->stack_b[index - 1];
+		index--;
+	}
+	data->stack_b[0] = data->stack_a[0];
+	data->stack_size_b++;
+	index = 0;
+	while (index < data->stack_size_a)
+	{
+		data->stack_a[index] = data->stack_a[index + 1];
+		index++;
+	}
+	data->stack_size_a--;
+	return (1);
+}
+
 int	push_a(t_data *data)
 {
+	int	index;
 
+	if (data->stack_size_b == 0)
+		return (0);
+	index = data->stack_size_a;
+	while (index > 0)
+	{
+		data->stack_a[index] = data->stack_a[index - 1];
+		index--;
+	}
+	data->stack_a[0] = data->stack_b[0];
+	data->stack_size_a++;
+	index = 0;
+	while (index < data->stack_size_b)
+	{
+		data->stack_b[index] = data->stack_b[index + 1];
+		index++;
+	}
+	data->stack_size_b--;
+	return (1);
+}
+
+int	rotate_b(t_data *data)
+{
+	int	index;
+	int	temp;
+
+	temp = data->stack_b[0];
+	index = 0;
+	while (index < data->stack_size_b)
+	{
+		data->stack_b[index] = data->stack_b[index + 1];
+		index++;
+	}
+	data->stack_b[data->stack_size_b - 1] = temp;
+	return (1);
+}
+
+int	rotate_a(t_data *data)
+{
+	int	index;
+	int	temp;
+
+	temp = data->stack_a[0];
+	index = 0;
+	while (index < data->stack_size_a)
+	{
+		data->stack_a[index] = data->stack_a[index + 1];
+		index++;
+	}
+	data->stack_a[data->stack_size_a - 1] = temp;
 	return (1);
 }
 
@@ -73,7 +153,9 @@ int	parse_stacks(t_data *data, size_t size, char *argv[])
 {
 	size_t index;
 
-	data->stack_size = size;
+	data->stack_size_total = size;
+	data->stack_size_a = size;
+	data->stack_size_b = 0;
 	data->stack_a = ft_calloc(size, sizeof(int));
 	data->stack_b = ft_calloc(size, sizeof(int));
 	index = 0;
--- push_swap.h
+++ push_swap.h
@@ -9,7 +9,9 @@ typedef struct s_data
 {
 	int	*stack_a;
 	int	*stack_b;
-	int	stack_size;
+	int	stack_size_total;
+	int	stack_size_a;
+	int	stack_size_b;
 }			   t_data;
 
 int	error(char *error_message);
@@ -19,5 +21,11 @@ int	swap_a(t_data *data);
 int	swap_b(t_data *data);
 
 int	swap_a_b(t_data *data);
+
+int	push_a(t_data *data);
+
+int	push_b(t_data *data);
+
+int	rotate_a(t_data *data);
 
 #endif
